4da4a7ef991458469a5e7ed4f9bef10befc83c6b
赵一欢 <happy@sdblo.com>
2018-02-01 16:45:46 +0800

    NewTab

---
+++ RossiHD/RossiHD/ViewController/RootTabBar/RootTabBarViewController.swift
@@ -0,0 +1,35 @@
+//
+//  RootTabBarViewController.swift
+//  RossiHD
+//
+//  Created by 赵一欢 on 2018/2/1.
+//  Copyright © 2018年 zhaoyihuan. All rights reserved.
+//
+
+import UIKit
+
+class RootTabBarViewController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.tabBar.isHidden = true;
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
